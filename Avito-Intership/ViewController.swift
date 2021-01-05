4cef1ea3b861a6e49838ee4799f1c56cd42cfd49
Malyshev Maksim <malyshev.maksim99@gmail.com>
2021-01-05 18:52:22 +0500

    Initial Commit

---
+++ Avito-Intership/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avito-Intership
+//
+//  Created by Малышев Максим Алексеевич on 1/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
